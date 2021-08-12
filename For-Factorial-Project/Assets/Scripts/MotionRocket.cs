8d840311616939cac45c1d7f03ea0344be6d3f81
Onur Gedik <gazi.onur98@gmail.com>
2021-08-12 22:55:08 +0300

    RocketmanAni

--- For-Factorial-Project/Assets/Scripts/MotionRocket.cs
+++ For-Factorial-Project/Assets/Scripts/MotionRocket.cs
@@ -6,31 +6,44 @@ public class MotionRocket : MonoBehaviour
 {
     public Animator animator;
 
-    
+    public Animator animator2;
 
     private float timeOfAni(float time)
     {
-        return Mathf.Clamp(time, 0f, 1f);
+        return Mathf.Clamp(time, 0f, 0.99f);
     }
 
+    
+    
     public void SlideRight()
     {
-       // animator.SetFloat("time", timeOfAni(animator.GetFloat("time") - Time.deltaTime));
+      
+         animator2.SetFloat("direction", timeOfAni(animator2.GetFloat("direction") - Time.deltaTime*2));
+
+       
+
 
     }
 
     public void SlideLeft()
     {
-       // animator.SetFloat("time", timeOfAni(animator.GetFloat("time") + Time.deltaTime));
+        
+        animator2.SetFloat("direction", timeOfAni(animator2.GetFloat("direction") + Time.deltaTime * 2));
 
     }
 
     public void NoMoving()
     {
-       // animator.SetFloat("time", timeOfAni(animator.GetFloat("time")));
+        animator.SetFloat("time", timeOfAni(animator.GetFloat("time") + Time.deltaTime*5));
+        animator2.SetFloat("direction", Mathf.Lerp(animator2.GetFloat("direction"), 0.5f, Time.deltaTime));
     }
 
     public void NoTouching()
     {
+        
+          animator2.SetFloat("direction", 0.5f);
+
+      animator.SetFloat("time", timeOfAni(animator.GetFloat("time") - Time.deltaTime*5));
+        
     }
 }
